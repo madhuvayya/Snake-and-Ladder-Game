86fe215ce6058694f5cf6253d058163c44be93ba
madhuvayya <madhu.vayya@gmail.com>
2020-04-16 17:18:18 +0530

    Added if statement to check exact win position

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -1,17 +1,20 @@
 #!/bin/bash -x
 
 WIN_POS=100
+
 playerPos=0
 
 while true
 do
 	random=$((1+RANDOM%6))
 	option=$((RANDOM%3))
+	prevPos=0
 
 	case $option in
 		0 )
 			;;
 		1 )
+			prevPos=$playerPos
 			playerPos=$(($playerPos+$random))
 			;;
 		2 )
@@ -22,6 +25,9 @@ do
 	if [ $playerPos -lt 0 ]
 	then
 		playerPos=0
+	elif [ $playerPos -gt 100 ]
+	then
+		playerPos=$prevPos
 	fi
 
 	if [ $playerPos -eq $WIN_POS ]
