792c8bc5d2a58469f98be689a2fd8fb20ba20b96
madhuvayya <madhu.vayya@gmail.com>
2020-04-16 16:27:46 +0530

    Added case statement to check noplay,ladder and snake options

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -3,5 +3,19 @@
 playerPos=0
 
 random=$((1+RANDOM%6))
+option=$((RANDOM%3))
+echo option $option
 
-echo $random
+case $option in
+	0 )
+		echo "NO play stay in same position"
+		;;
+	1 )
+		playerPos=$(($playerPos+$random))
+		;;
+	2 )
+		playerPos=$(($playerPos-$random))
+		;;
+esac
+
+echo "player position" $playerPos
